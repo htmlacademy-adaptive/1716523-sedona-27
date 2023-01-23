0e3906f784fd8da227cb00e37d5bbd0ebacd0f4a
Vladimir <yaschuk_vladimir@mail.ru>
2023-01-24 00:16:51 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,78 +3,172 @@ import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
-import imagemin from 'gulp-imagemin';
 import csso from 'postcss-csso';
-import webp from 'gulp-webp';
+import rename from 'gulp-rename';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import del from 'del';
+import browser from 'browser-sync';
 
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
-    .pipe(postcss([
-      csso()
-    ]))
-    .pipe(gulp.dest('build/css'))
-    .pipe(browser.stream());
+return gulp.src('source/sass/style.scss', { sourcemaps: true })
+.pipe(plumber())
+.pipe(sass().on('error', sass.logError))
+.pipe(postcss([
+autoprefixer(),
+csso()
+]))
+.pipe(rename('style.min.css'))
+.pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+.pipe(browser.stream());
 }
 
-//optimaze images
+// HTML
 
-export const images = () => {
-  return gulp.src('source/img/**/*.{jpg,png}')
-  .pipe(imagemin())
-  .pipe(gulp.dest('build/img'))
+const html = () => {
+return gulp.src('source/*.html')
+.pipe(gulp.dest('build'));
 }
 
-export const createWebp = () => {
-  return gulp.src("source/img/**/*.{jpg,png}")
-  .pipe(webp({quality: 90}))
-  .pipe(gulp.dest("build/img"))
+// Scripts
+
+const scripts = () => {
+return gulp.src('source/js/script.js')
+.pipe(gulp.dest('build/js'))
+.pipe(browser.stream());
 }
 
-//copy svg sprite
+// Images
 
-export const sprite = () => {
-  return gulp.src("source/img/sprite.svg")
-  .pipe(gulp.dest("build/img"))
+const optimizeImages = () => {
+return gulp.src('source/img/**/*.{png,jpg}')
+.pipe(squoosh())
+.pipe(gulp.dest('build/img'))
 }
 
-//copy files
+const copyImages = () => {
+return gulp.src('source/img/**/*.{png,jpg}')
+.pipe(gulp.dest('build/img'))
+}
 
-export const copy = () => {
-  return gulp.src('source/*.html')
+// WebP
+
+const createWebp = () => {
+return gulp.src('source/img/**/*.{png,jpg}')
+.pipe(squoosh({
+webp: {}
+}))
+.pipe(gulp.dest('build/img'))
+}
+
+// SVG
+
+const svg = () =>
+gulp.src(['source/img/*.svg', '!source/img/sprite.svg'])
+.pipe(svgo())
+.pipe(gulp.dest('build/img'));
+
+const sprite = () => {
+return gulp.src('source/img/sprite.svg')
+.pipe(rename('sprite.svg'))
+.pipe(gulp.dest('build/img'));
+}
+
+// Copy
+
+const copy = (done) => {
+gulp.src([
+'source/fonts/*.{woff2,woff}',
+'source/img/favicons/icon.svg'
+], {
+base: 'source'
+})
+.pipe(gulp.dest('build'))
+done();
+}
+
+// favicon
+
+const favicon = (done) => {
+  gulp.src(['favicon.ico',
+  'manifest.webmanifest'])
   .pipe(gulp.dest('build'))
+  done();
 }
+
+// Clean
+
+export const clean = () => {
+return del('build');
+};
 
 // Server
 
 const server = (done) => {
-  browser.init({
-    server: {
-      baseDir: 'source'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
+browser.init({
+server: {
+baseDir: 'build'
+},
+cors: true,
+notify: false,
+ui: false,
+});
+done();
+}
+
+// Reload
+
+const reload = (done) => {
+browser.reload();
+done();
 }
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+gulp.watch('source/js/script.js', gulp.series(scripts));
+gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
+// Build
+
+export const build = gulp.series(
+clean,
+copy,
+favicon,
+optimizeImages,
+gulp.parallel(
+styles,
+html,
+scripts,
+svg,
+sprite,
+favicon,
+createWebp
+),
+);
+
+// Default
 
 export default gulp.series(
-  styles, server, watcher
-);
+clean,
+copy,
+copyImages,
+gulp.parallel(
+styles,
+html,
+scripts,
+svg,
+sprite,
+favicon,
+createWebp
+),
+gulp.series(
+server,
+watcher
+));
