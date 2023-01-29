da78f15179e6dd74746fedbf3b3468603f8b2d11
Vladimir <yaschuk_vladimir@mail.ru>
2023-01-29 22:15:50 +0300

    corrects mobile menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.getElementById("burger");
   const navToggle = document.getElementById("nav-toggle")
+  navToggle.classList.remove("main-nav--nojs")
+  navToggle.classList.remove("main-nav--open")
   console.log(burger);
   console.log(navToggle);
   if (burger) {
     burger.addEventListener("click", () => {
       if (navToggle) {
-        navToggle.classList.toggle("open");
+        navToggle.classList.toggle("main-nav--open");
       }
     });
   }
